75f3858b3ec4c5176e67cc2b123146f0d6dbaa9c
rlacodud <rlacodud0313@naver.com>
2024-11-10 17:27:11 +0900

    build: react-query 적용

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,14 @@ import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { AppRoutes } from '@/router/index';
 import '@/styles/reset.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter(AppRoutes);
 
-createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />);
+createRoot(document.getElementById('root')!).render(
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>
+);
